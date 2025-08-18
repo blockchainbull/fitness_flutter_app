08971e437d27adade39df8ce293be11f58fe4c43
BlockchainBull.eth <40736637+blockchainbull@users.noreply.github.com>
2025-08-18 08:45:12 +0500

    setup render for backend

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "e22653ede92eaa0b547b48b0c8b2bc0c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "a596ea00fe469b10c109d2ad45358415",
+"flutter_bootstrap.js": "e48eace156769f825c77d726a24788a7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "89e60d4bdb0e2e227a394150f1436a17",
 "/": "89e60d4bdb0e2e227a394150f1436a17",
-"main.dart.js": "bea85d418b4e9adcd402439634597960",
+"main.dart.js": "9f188d9897041f04824c6fc1cbe2a03b",
 "manifest.json": "87958eee17845a0e7de35599d6ec6ceb",
 "version.json": "1cecd711698376e5b320fa6c8ccb572d"};
 // The application shell files that are downloaded before a service worker can
