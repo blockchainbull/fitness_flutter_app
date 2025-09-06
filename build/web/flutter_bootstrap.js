66a9f0450921b8ca23758784e3293743505c92a9
BlockchainBull.eth <40736637+blockchainbull@users.noreply.github.com>
2025-09-06 06:44:29 +0500

    new dashboard update

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3505034611"
+    serviceWorkerVersion: "2341750840"
   }
 });
