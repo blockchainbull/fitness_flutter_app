020f1c878c706723442978c891b5ba84cb5f3046
BlockchainBull.eth <40736637+blockchainbull@users.noreply.github.com>
2025-11-06 08:08:09 +0500

    web version

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a1fec36c45afaacdbcd9e4a88f1d640edb2a32a57943e4aaff6b50807c6e22f
-size 9590
+oid sha256:f22373d13d1a23a6a24a954e3adfe7b53a836986f64d81db7e6f60786ec367ec
+size 9588
